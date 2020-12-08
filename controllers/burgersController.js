a035939d2f89c40a7403a9812b7e5c43412b9e76
Michael Griffith <griffithcmichael@gmail.com>
2020-12-07 22:09:57 -0600

    pathing

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -3,11 +3,11 @@ var express = require("express");
 var router = express.Router();
 
 // Import the model (cat.js) to use its database functions.
-var cat = require("../models/burgers.js");
+var burger = require("../models/burgers.js");
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
-  cat.all(function(data) {
+  burger.all(function(data) {
     var hbsObject = {
       burgers: data
     };
