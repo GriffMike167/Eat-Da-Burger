3564a17ae775dc2e097f3ca8228338641df3f36f
Michael Griffith <griffithcmichael@gmail.com>
2020-12-08 13:36:04 -0600

    err code h10 cleared

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,18 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else {
 var connection = mysql.createConnection({
   host: "localhost",
-  port: 8080,
+  port: 3306,
   user: "root",
   password: "rootroot",
   database: "burgersDB"
 });
+};
 
 connection.connect(function(err) {
   if (err) {
